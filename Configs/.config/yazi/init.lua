b02f48a93766a6bb256c3e9734712488d8dd2021
angel-cursed <130891386+angel-cursed@users.noreply.github.com>
2024-12-22 12:13:28 +0100

    Delete Configs/.config/yazi directory

--- Configs/.config/yazi/init.lua
+++
@@ -1,6 +0,0 @@
-function Linemode:custom()
-    local size = self._file:size()
-    local owner = self._file:owner()
-    return ui.Line(string.format(owner, size and ya.readable_size(size):gsub(" ", "") or "-"))
-end
-
