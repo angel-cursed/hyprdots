178a227990a1085b39a92fdc484dfd2ff381b77e
angel-cursed <130891386+angel-cursed@users.noreply.github.com>
2025-06-25 20:17:23 +0200

    Delete Configs/.config/nvim directory

--- Configs/.config/nvim/lua/plugins/nerd-icons.lua
+++
@@ -1,8 +0,0 @@
-return {
-	"glepnir/nerdicons.nvim",
-	lazy = false,
-	config = function()
-		require("nerdicons").setup({})
-		vim.keymap.set("n", "<leader>ni", "<Cmd>NerdIcons<CR>", { desc = "Select nerd icon" })
-	end,
-}
